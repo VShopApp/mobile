2af8572cdc1af4b92744cd95083894ce72e38ae9
vasc <nicovanaschaufler@gmail.com>
2021-09-26 21:58:08 +0200

    update theme & branding

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { SafeAreaView, StatusBar } from "react-native";
 import Login from "./components/Login";
-import { Provider as PaperProvider } from "react-native-paper";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
 import Shop from "./components/Shop";
 import AppBar from "./components/AppBar";
 import Settings from "./components/Settings";
@@ -14,8 +14,18 @@ export default function App() {
   const [snackbarVisible, setSnackbarVisible] = useState(false);
   const [snackbarTxt, setSnackbarTxt] = useState("");
 
+  const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+      ...DefaultTheme.colors,
+      primary: "#fa4454",
+      accent: "#042e27",
+    },
+  };
+
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <SafeAreaView style={{ width: "100%", height: "100%" }}>
         <AppBar setView={setView} showActions={user != null && !user.loading} />
         {!user || user.loading ? (
