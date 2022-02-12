77bdfb61e0da12d02b8d2dc38edd7db06a554fb2
vascYT <nicovanaschaufler@gmail.com>
2022-02-12 14:16:24 +0100

    force update

--- components/Update.tsx
+++ components/Update.tsx
@@ -11,31 +11,23 @@ interface props {}
 export default function Update(props: PropsWithChildren<props>) {
   const [visible, setVisible] = useState(false);
 
-  const reloadApp = async () => {
-    await Updates.reloadAsync();
-  };
-
   Updates.addListener(async (event) => {
     // Reload app once update is downloaded
     if (event.type === Updates.UpdateEventType.UPDATE_AVAILABLE) {
       setVisible(true);
+      await Updates.reloadAsync();
     }
   });
 
   return (
     <>
       <Portal>
-        <PaperDialog visible={visible} onDismiss={reloadApp}>
-          <PaperDialog.Title>Update available</PaperDialog.Title>
+        <PaperDialog visible={visible} dismissable={false}>
           <PaperDialog.Content>
             <Paragraph>
-              A new update has been downloaded. The app needs to be restarted
-              for the changes to take effect.
+              An update has been downloaded, relaunching app...
             </Paragraph>
           </PaperDialog.Content>
-          <PaperDialog.Actions>
-            <Button onPress={reloadApp}>OKAY</Button>
-          </PaperDialog.Actions>
         </PaperDialog>
       </Portal>
     </>
